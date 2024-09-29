c6cc751c03f4fb3abf3dead87471cdf8ab6e4769
Joswin_Shogi <104709945+joswinshogi@users.noreply.github.com>
2024-09-29 18:53:49 +0530

    Refactor file structure and include missing end of file newline

--- Project/models/product.js
+++ Project/models/product.js
@@ -2,39 +2,36 @@ const fs = require('fs');
 const path = require('path');
 
 const p = path.join(
-        path.dirname(process.mainModule.filename),
-        'data',
-        'products.json'
-)
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
 
-const getProductsFromFile = (cb) => {
-        
-        fs.readFile(p,(err,fileContent) => {
-            if(err) {
-                return cb([])
-            }
-            cb(JSON.parse(fileContent))
-        })
-}
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
 
 module.exports = class Product {
-    constructor(title) {
-        this.title = title;
-    }
+  constructor(t) {
+    this.title = t;
+  }
 
-    save() {
-        getProductsFromFile(products => {
-            products.push(this);
-            fs.writeFile(p, JSON.stringify(products), (err) => {
-                console.log(err);
-            });
-        })
+  save() {
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
 
-    }
-
-    static fetchAll(cb) {
-        getProductsFromFile(cb);
-    }
-
-    
-}
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+};
